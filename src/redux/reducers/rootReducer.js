5b81b7b3c92a5e6c9a50203ebace0f7a2d452741
Andrii Levchenko <sanyoksanyok@i.ua>
2021-02-12 22:06:00 +0200

    notes taken into account2

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,13 +2,11 @@ import {combineReducers} from 'redux';
 import tasksReducer from './tasksReducer';
 import createReducer from './createReducer';
 import authReducer from './authReducer';
-import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
 	tasksReducer,
 	createReducer,
 	authReducer,
-	form: formReducer
 });
 
 
